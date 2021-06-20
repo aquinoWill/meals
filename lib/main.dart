c9c9c6b059bdbeb48ae7794960592a14a2625368
Willian Aquino <aquino.will@gmail.com>
2021-06-19 21:27:55 -0300

    Config TabBar to render page Home and Details

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'utils/app_routes.dart';
-import 'screens/categories_screen.dart';
-import 'screens/categorys_meals_screen.dart';
+import 'screens/tabs_screen.dart';
 import 'screens/meal_detail_screen.dart';
+import 'screens/categorys_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,20 +10,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
+      title: 'Delivery Meals',
       theme: ThemeData(
         primarySwatch: Colors.pink,
         accentColor: Colors.amber,
         fontFamily: 'Raleway',
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         textTheme: ThemeData.light().textTheme.copyWith(
-          bodyText2: TextStyle(
-            fontSize: 20,
-            fontFamily: 'RobotoCondensed',
-          )),
+                bodyText2: TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+            )),
       ),
       routes: {
-        AppRoutes.HOME: (ctx) => CategoriesScreen(),
+        AppRoutes.HOME: (ctx) => TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
         AppRoutes.MEALS_DETAIL: (ctx) => MealDetailsScreen(),
       },
